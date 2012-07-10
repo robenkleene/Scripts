b5fd72a1a6afd27c798b5648d83462d2f30bb25c
Roben Kleene <robenk@gmail.com>
2012-07-10 11:04:45 -0400

    Added install scripts and adapted mdprev to use lynx

--- mdprev.sh
+++ mdprev.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
-TEMPDIRECTORY=$(mktemp -d /tmp/markdown-XXXXXX)
-TEMPFILE=$TEMPDIRECTORY/output.html
-markdown "$1" | smartypants > $TEMPFILE && open $TEMPFILE
+# TEMPDIRECTORY=$(mktemp -d /tmp/markdown-XXXXXX)
+# TEMPFILE=$TEMPDIRECTORY/output.html
+# markdown "$1" | smartypants > $TEMPFILE && open $TEMPFILE
+markdown "$1" | smartypants | lynx -stdin
