f181e983c191505724ac3dfbadc3fa9debc1f5f5
Roben Kleene <robenk@gmail.com>
2012-07-10 17:04:19 -0400

    Trying to clean up

--- markdownpreview.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# TEMPDIRECTORY=$(mktemp -d /tmp/markdown-XXXXXX)
-# TEMPFILE=$TEMPDIRECTORY/output.html
-# markdown "$1" | smartypants > $TEMPFILE && open $TEMPFILE
-
-markdown $1 | smartypants | lynx -stdin
