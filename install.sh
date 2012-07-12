68af86953f4860733384d01f828409bbeac90df6
Roben Kleene <robenk@gmail.com>
2012-07-11 23:04:12 -0400

    Added ninbox.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ ln -s ~/Scripts/mdprev.sh ~/Scripts/bin/mdprev
 ln -s ~/Scripts/mdquote.sh ~/Scripts/bin/mdquote
 ln -s ~/Scripts/runjs.sh ~/Scripts/bin/runjs
 ln -s ~/Scripts/fcontents.sh ~/Scripts/bin/fcontents
+ln -s ~/Scripts/ninbox.sh ~/Scripts/bin/ninbox
