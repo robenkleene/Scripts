010ced921ce66d19a3da1b40ae47ba1b92d434d9
Roben Kleene <robenk@gmail.com>
2012-07-14 20:49:39 -0400

    Made install script install iteratively instead of needing to be updated manually

--- install.sh
+++ install.sh
@@ -1,6 +1,30 @@
-ln -s ~/Scripts/mdprev.sh ~/Scripts/bin/mdprev
-ln -s ~/Scripts/mdquote.sh ~/Scripts/bin/mdquote
-ln -s ~/Scripts/runjs.sh ~/Scripts/bin/runjs
-ln -s ~/Scripts/fname.sh ~/Scripts/bin/fname
-ln -s ~/Scripts/ftext.sh ~/Scripts/bin/ftext
-ln -s ~/Scripts/ninbox.sh ~/Scripts/bin/ninbox
+#!/bin/sh
+
+INSTALL_DIRECTORY_NAME="Scripts" # The name of the directory to install from.
+
+# Stop if this is being run from any directory besides the install directory
+DIRECTORY=${PWD}
+DIRECTORY_NAME=${PWD##*/}
+if [ ! $DIRECTORY_NAME == $INSTALL_DIRECTORY_NAME ]; then
+	echo "ERROR: This directory \"$DIRECTORY_NAME\" doesn't match $INSTALL_DIRECTORY_NAME."
+	echo "This script only runs from the $INSTALL_DIRECTORY_NAME directory."
+	exit 1
+fi
+
+function MakeSymlink {
+	FILENAME=$1
+  	EXECUTABLE_NAME="${FILENAME%.*}"
+ 	DESTINATION=$DIRECTORY/bin/$EXECUTABLE_NAME
+	if [ -f $DESTINATION ] || [ -d $DESTINATION ]; then
+		echo "$DESTINATION skipped because it already exists."
+	else
+		ln -s $DIRECTORY/$thisFILE $DESTINATION
+	fi	
+}
+
+for thisFILE in *; do 
+	# Exclude this install script and directories
+ 	if [[ ! $thisFILE =~ "install.sh" ]] && [ ! -d $thisFILE ]; then
+		MakeSymlink $thisFILE
+ 	fi
+done
