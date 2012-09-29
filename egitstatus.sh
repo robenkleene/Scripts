f88dec640f365018e5f257a4ce9a707dfeb57efe
Roben Kleene <robenk@gmail.com>
2012-09-29 17:34:57 -0400

    Added quick script for checking statuses of OS environment repos

---
+++ egitstatus.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ~/Library/Services/
+pwd
+git status
+cd ~/Library/Application\ Support/Avian/Bundles/Roben.tmbundle/
+pwd
+git status
+cd ~/Library/Scripts/
+pwd
+git status
+cd ~/Development/Scripts/
+pwd
+git status
+cd ~/Development/Snippets/
+pwd
+git status
+cd ~/Development/Bookmarklets/
+pwd
+git status
