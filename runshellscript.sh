a33e8c1ad9824914de71eaa0c580be32cc91c27d
Roben Kleene <robenk@gmail.com>
2013-03-31 19:17:44 -0400

    Making runshellscript work with terminal wrapper

--- runshellscript.sh
+++ runshellscript.sh
@@ -1,10 +1,41 @@
 #!/bin/sh
 
+# Have to strip the first line if I found a command
+
+usage () {
+	echo "Runs a script passed to it by interpreting the shebang."
+	echo
+	echo "-e : Echo the command that would be executed to STDIN rather than running it."
+}
+
+ECHO=false
+while getopts eh option
+do
+	case "$option"
+	in
+	    e)  ECHO=true
+			;;
+	    h)  usage
+	        exit 0 
+	        ;;
+	    \?) echo "Invalid option or missing argument"
+			usage
+	        exit 1
+	        ;;
+	esac
+done
+
 INPUT=`cat`
-COMMAND=`echo "$INPUT" | perl -ne '/\A#!(\/.*)$/; print \$1; exit'`
-if [[ -z "$COMMAND" ]]; then
-	COMMAND=sh
+SHEBANG=`echo "$INPUT" | perl -ne '/\A#!(\/.*)$/; print \$1; exit'`
+if [[ -z "$SHEBANG" ]]; then
+	# If no shebang was found, use sh
+	SHEBANG=sh
+else
+	# Otherwise, strip the shebang line
+	INPUT=`echo "$INPUT" | sed '1d'`
 fi
+
+
 
 # exit 1 if the input contains the SCRIPT token
 if [[ $INPUT =~ SCRIPT ]]; then
@@ -12,6 +43,13 @@ if [[ $INPUT =~ SCRIPT ]]; then
     exit 1
 fi
 
-$COMMAND <<-SCRIPT
-$INPUT
-SCRIPT
+if $ECHO; then
+	echo "$SHEBANG <<-SCRIPT"
+	echo "$INPUT"
+	echo "SCRIPT"
+else
+	$SHEBANG <<-SCRIPT
+	$INPUT
+	SCRIPT
+fi
+
