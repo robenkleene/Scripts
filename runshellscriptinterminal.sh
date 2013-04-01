b2cf9b1ceb08d4097783c73a1c432870c19a006d
Roben Kleene <robenk@gmail.com>
2013-03-31 20:24:19 -0400

    runshellscriptinterminal

--- runshellscriptinterminal.sh
+++ runshellscriptinterminal.sh
@@ -2,9 +2,15 @@
 
 SCRIPT=`runshellscript -e | sed 's/"/\\\"/g'`
 
+if [[ $SCRIPT =~ "ERROR: Script cannot contain the SCRIPT token." ]]; then
+    echo "ERROR: Script cannot contain the SCRIPT token."
+    exit 1
+fi
+
 osascript <<-APPLESCRIPT
 tell application "Terminal"
 	do script "$SCRIPT"
+	activate
 end tell
 return -- Suppresses superfluous output
 APPLESCRIPT
