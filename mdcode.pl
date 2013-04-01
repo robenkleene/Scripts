a05bfca43faad3e9993bc86016fa3cdc7f899731
Roben Kleene <robenk@gmail.com>
2013-03-31 20:06:06 -0400

    mdcode

---
+++ mdcode.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+my $mode;
+
+sub find_mode_from_line {
+	/(`|^\t)/;
+	$mode = $1;
+}
+
+while (<>) {
+	if (!defined $mode) { 
+		&find_mode_from_line($_);
+	}
+
+	if ($mode =~ /^\t/) {
+		if (/^\t(.*)$/) {
+			print "$1\n";
+		} else {
+			last;
+		}		
+	} elsif ($mode =~ /`/) {
+		/`(.*)`/;
+		print $1;
+		last;
+	}
+}
