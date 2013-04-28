c4cfd7f8407c7eeca5fa40f77a01b70971edced1
Roben Kleene <robenk@gmail.com>
2013-04-28 18:03:52 -0400

    snpt can now accept language as an argument

--- snpt.sh
+++ snpt.sh
@@ -4,21 +4,52 @@ SNIPPETS_DIRECTORY=~/Development/Snippets
 
 eval SNIPPETS_DIRECTORY_EXPANDED=${SNIPPETS_DIRECTORY}
 if [ ! -d "$SNIPPETS_DIRECTORY_EXPANDED" ]; then
-	echo "Error: $SNIPPETS_DIRECTORY_EXPANDED is not a directory"
-	exit 1
+    echo "Error: $SNIPPETS_DIRECTORY_EXPANDED is not a directory"
+    exit 1
 fi
 
-if [ -z "$1" ]; then
-	# If no arguement is supplied read one line from stdin
-	read TEMPLATE
+usage () {
+    echo "Usage: snpt [-l language] snippet"
+}
+
+while getopts l:h option
+do
+    case "$option"
+	in
+	l)  LANGUAGE=$OPTARG
+	    ;;
+	h)  usage
+	    exit 0 
+	    ;;
+	:)  usage # Error for missing value after arguement
+	    exit 1
+	    ;;
+	\?) usage
+	    exit 1
+	    ;;
+    esac
+done
+
+if [[ -z "$LANGUAGE" ]]; then
+    SNIPPET=$1
 else
-	TEMPLATE=$1
+    SNIPPET=$3
 fi
 
-MATCH=$(find $SNIPPETS_DIRECTORY -iname "$TEMPLATE" -print -quit)
+if [ -z "$SNIPPET" ]; then
+    # If no snippet was supplied as an argument, a read one line from stdin
+    read SNIPPET
+fi
+
+if [[ ! -z "$LANGUAGE" ]]; then
+    SNIPPET=$SNIPPET.$LANGUAGE
+fi
+
+
+MATCH=$(find $SNIPPETS_DIRECTORY -iname "$SNIPPET" -print -quit)
 
 if [ -z "$MATCH" ]; then
-	echo "No snippet found for \"$TEMPLATE\""
+	echo "No snippet found for \"$SNIPPET\""
 	exit 1
 fi
 
