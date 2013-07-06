67e027b5c330a5b136cfe5facc7ec9aa5191914e
Roben Kleene <robenk@gmail.com>
2013-07-06 12:50:31 -0400

    wcack

---
+++ wcack.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'webconsole'
+
+PLUGIN_NAME = "Ack"
+
+term = ARGV[0]
+directory = ARGV[1]
+
+if !directory 
+  directory = Dir.pwd
+end
+
+WebConsole::run_plugin(PLUGIN_NAME, [term], directory)
