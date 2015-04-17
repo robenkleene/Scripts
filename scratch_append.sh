559df8c2451ca7cf4c9004b82dd37d39fdb340b2
Roben Kleene <robenk@gmail.com>
2015-04-16 20:42:57 -0400

    Scratch append

--- scratch_append.sh
+++ scratch_append.sh
@@ -3,4 +3,8 @@
 cd ~/Scratch/
 scratch_file=`ls -t $PWD/*.* | head -n1 | xargs echo -n`
 
-{ echo ; echo ; cat - ; } >> "$scratch_file"
+read first_line
+
+{ echo ; echo $first_line; cat - ; } >> "$scratch_file"
+
+osascript -e "display notification \"$first_line\" with title \"Append to Scratch\""
