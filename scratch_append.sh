66b05095fa160a0f4dc334e2006fa5dcc00dabe5
Roben Kleene <robenk@gmail.com>
2015-05-05 20:22:34 -0400

    Todo

--- scratch_append.sh
+++ scratch_append.sh
@@ -7,4 +7,6 @@ read first_line
 
 { echo ; echo $first_line; cat - ; } >> "$scratch_file"
 
+# TODO This will fail if `$first_line` has characters that need to be escaped
+
 osascript -e "display notification \"$first_line\" with title \"Append to Scratch\""
