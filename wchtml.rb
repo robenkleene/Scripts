8ced140d99fb39b4a1519f1a17abbac87d681e29
Roben Kleene <robenk@gmail.com>
2015-08-18 20:55:22 -0400

    Fix typo

--- wchtml.rb
+++ wchtml.rb
@@ -12,7 +12,7 @@ end
 file = ARGF.file
 file_path = File.expand_path(file)
 
-# TODO Specifying a directory here event though it isn't necessary for this plugin is sort of inelegant, perhaps running a plugin shouldn't require a directory? What would the intelligent default be? Probably the plugins default directory?
+# TODO Specifying a directory here even though it isn't necessary for this plugin is sort of inelegant, perhaps running a plugin shouldn't require a directory? What would the intelligent default be? Probably the plugins default directory?
 directory = Dir.pwd
 
 WebConsole::run_plugin(PLUGIN_NAME, directory, [file_path])
