f785cfaf39e9b3cf5b341f13b0855ab5ec9412a0
Roben Kleene <robenk@gmail.com>
2017-01-02 19:01:05 -0500

    Finish nvimedit

--- nvimedit.py
+++ nvimedit.py
@@ -7,36 +7,16 @@ import sys
 from neovim import attach
 
 args = sys.argv[1:]
-if not args:
-    print("Usage: {} <filename> ...".format(sys.argv[0]))
-    sys.exit(1)
 
 addr = os.environ.get("NVIM_LISTEN_ADDRESS", None)
 if not addr:
+    # Just use `nvim` if no host exists
     os.execvp('nvim', args)
 
 nvim = attach("socket", path=addr)
-
 ldir = os.getcwd()
 
-def _setup():
-    nvim.input('<c-\\><c-n>')  # exit terminal mode
-    chid = nvim.channel_id
-    # nvim.command('augroup EDIT')
-    # nvim.command('au BufEnter <buffer> call rpcnotify({0}, "n")'.format(chid))
-    # nvim.command('au BufEnter <buffer> startinsert'.format(chid))
-    # nvim.command('augroup END')
-    nvim.vars['files_to_edit'] = args
-    for x in args:
-        nvim.command('exe "drop ".remove(g:files_to_edit, 0)')
-
-
-def _exit(*args):
-    nvim.vars['files_to_edit'] = None
-    nvim.command('augroup EDIT')
-    nvim.command('au!')
-    nvim.command('augroup END')
-    nvim.stop_loop()
-
-
-nvim.run_loop(_exit, _exit, _setup)
+nvim.input('<c-\\><c-n>')  # exit terminal mode
+nvim.command('exe "lcd " fnameescape("' + ldir + '")')
+for x in args:
+    nvim.command('exe "drop " fnameescape("' + x + '")')
