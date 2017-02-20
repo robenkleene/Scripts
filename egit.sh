f425ca68df40b3b2c73894545c140284febd7e79
Roben Kleene <robenk@gmail.com>
2017-02-20 16:55:57 -0500

    Add scratch to egit

--- egit.sh
+++ egit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EGITREPOS=~/Development/Dotfiles/:~/Development/Scripts/:~/Development/Snippets/:~/Development/Archive/:~/Development/Settings/:~/Documentation/design-references/:~/Documentation/development-references/:~/Documentation/music-production-references/:~/Documentation/software-references/
+EGITREPOS=~/Development/Dotfiles/:~/Development/Scripts/:~/Development/Snippets/:~/Development/Archive/:~/Development/Settings/:~/Documentation/design-references/:~/Documentation/development-references/:~/Documentation/music-production-references/:~/Documentation/software-references/:~/Development/Scratch/
 
 if [ "$(uname)" == "Darwin" ]; then
 	EGITREPOS=$EGITREPOS:~/Library/Services/:~/Library/Application\ Support/TextMate/Bundles/Roben\ Kleene.tmbundle/:~/Library/Scripts/:~/Library/Script\ Libraries/
