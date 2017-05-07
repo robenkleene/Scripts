66108c30f15b4f385c3b494ca0781969a54f3b66
Roben Kleene <robenk@gmail.com>
2017-05-07 12:49:59 -0400

    Make title script support hyphenated filenames

--- markdown-title.sh
+++ markdown-title.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-FILENAME=$(basename "$1")
-FILENAMENOEXT=${FILENAME%.*}
-echo "# $FILENAMENOEXT"
+filename=$(basename "$1")
+filename_no_ext=${filename%.*}
+if [[ $filename_no_ext =~ [a-z-]* ]]; then
+  # If the filename contains no uppercase letters than assume it's a lowercase,
+  # hypenated filename and convert it to title case with spaces.
+  title=$(echo $filename_no_ext | tr "-" " " | title-case)
+fi
+echo "# $title"
 echo
