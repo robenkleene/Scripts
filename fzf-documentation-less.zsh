6d7bc80e938a168990fff1bdded5438aa0447268
Roben Kleene <robenk@gmail.com>
2017-05-12 12:06:46 -0400

    Add doc command

---
+++ fzf-documentation-less.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+source ~/Development/Scripts/nobin/_fzf-inline-result.sh
+
+cd ~/Documentation/
+local result=$(_fzf-inline-result)
+if [[ -n $result ]]; then
+  parameter=$(printf '%q' "$PWD/$result")
+  final_cmd="cat $parameter | less"
+  eval $final_cmd
+  if [ $? -eq 0 ]; then
+    # Add to history
+    print -sr $final_cmd
+  fi
+fi
