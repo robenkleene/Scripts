91953fb503172bee8b5c12b31c620b605226bd1d
Roben Kleene <robenk@gmail.com>
2017-06-20 22:24:42 -0400

    Improve backup-text

--- backup-text.sh
+++ backup-text.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-text=`cat`
-printf "%q" $text | automator -i - ~/Library/Services/Backup\ Text.workflow
+backup_root_directory=~/Archive/Text/
+today=`date +%Y-%B-%d`
+backup_directory=$backup_root_directory$today
+mkdir -p $backup_directory
+archive_file=`mktemp $backup_directory/$today-ArchivedText.XXXX`
+cat > $archive_file
+destination_archive_file=$archive_file.txt
+mv -n $archive_file $destination_archive_file
+if [ $? -eq 0 ]; then
+  echo $destination_archive_file
+else
+  echo $archive_file
+fi
