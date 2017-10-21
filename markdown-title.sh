bd8d97b6d9c5183332deb701a24996094b0f2f5c
Roben Kleene <robenk@gmail.com>
2017-10-21 14:18:21 -0400

    Add rename title case

--- markdown-title.sh
+++ markdown-title.sh
@@ -2,10 +2,11 @@
 
 filename=$(basename "$1")
 filename_no_ext=${filename%.*}
-if [[ $filename_no_ext =~ [a-z-]* ]]; then
-  # If the filename contains no uppercase letters than assume it's a lowercase,
-  # hypenated filename and convert it to title case with spaces.
-  title=$(echo $filename_no_ext | tr "-" " " | title-case)
+if [[ $filename_no_ext =~ ^[a-z0-9-]*$ ]]; then
+    # If the filename contains only lowercase letters and hyphens then assume
+    # convert it to title case with spaces.
+  script_dir=$(dirname $0)
+  title=$(echo $filename_no_ext | tr "-" " " | $script_dir/title-case)
 fi
 echo "# $title"
 echo
---
+++ rename-title-case.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+for i in "$@"; do
+  filename=$(basename "$1")
+  filename_no_ext=${filename%.*}
+if [[ $filename_no_ext =~ ^[a-z0-9-]*$ ]]; then
+    # If the filename contains only lowercase letters and hyphens then assume
+    # convert it to title case with spaces.
+    script_dir=$(dirname $0)
+    title=$(echo $filename_no_ext | tr "-" " " | $script_dir/title-case)
+    extension="${filename##*.}"
+    newfilename=$title.$extension
+    dir=$(dirname "$i")
+    newpath=$dir/$newfilename
+    mv "$i" "$newpath"
+  fi
+done
