cb9fd7db0a4818f5b847e5a363d9763c386cd759
Roben Kleene <robenk@gmail.com>
2017-11-12 15:54:24 -0500

    Support https

--- nobin/_urls-search.pl
+++ nobin/_urls-search.pl
@@ -3,8 +3,11 @@
 my $search = "http://www.google.com/search?q=";
 
 while (<>) {
+	# Remove `http` or `https`
 	$_ =~ s{http://}{}gi;
-	$_ =~ s{/.*}{}gi; # strip everything after first slash
+	$_ =~ s{https://}{}gi;
+	$_ =~ s{/.*}{}gi; # strip everything after first slash (only search the
+			  # base domain)
 	chomp($_);
 	$search .= "site:$_+OR+";
 }
