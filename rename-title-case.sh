6c87bcd24f9b83215ceef977ce1c5a3c77e84d3d
Roben Kleene <robenk@gmail.com>
2017-11-26 16:57:17 -0500

    Make rename work for directories
    Also fix issues for names without file extensions

--- rename-title-case.sh
+++ rename-title-case.sh
@@ -10,14 +10,28 @@ for i in "$@"; do
     # convert it to title case with spaces.
     script_dir=$(dirname $0)
     title=$(echo $filename_no_ext | tr "-" " " | $script_dir/title-case)
-    extension="${filename##*.}"
-    newfilename=$title.$extension
+    if [ "$filename_no_ext" == "$filename" ]; then
+      # If there was no extension (e.g., common for folders) the name just
+      # equals the title
+      newfilename=$title
+    else
+      # If there is an extension, re-add it
+      extension="${filename##*.}"
+      newfilename=$title.$extension
+    fi
     dir=$(dirname "$i")
     newpath=$dir/$newfilename
     tmppath=$(mktemp "$newpath.XXXXXX")
     # First move to a temp path, this allows capitalizing single word filenames
     # which would otherwise fail because of macOS case-sensitive file system
     # issues
+    if [ -d "$i" ]; then
+      # If it's a directory, first delete the `$tmppath`, otherwise the `mv`
+      # will fail because you can't move a directory onto a file.
+      # (Note that for files we create the `$tmppath` and then just overwrite
+      # it, which is a bit unintuitive.)
+      rm "$tmppath"
+    fi
     mv "$i" "$tmppath"
     mv -n "$tmppath" "$newpath"
   fi
