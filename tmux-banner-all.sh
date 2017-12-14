843c0a51ae314869099501afa19e3935a87690f6
Roben Kleene <robenk@gmail.com>
2017-12-14 09:49:47 -0500

    Don't banner unattached sessions

--- tmux-banner-all.sh
+++ tmux-banner-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in $(tmux list-sessions | cut -d: -f 1); do
+for i in $(tmux list-sessions -F "#{?session_attached,#S,}" | grep .); do
   # tmux send -t $i tmux-banner ENTER
   tmux run-shell -t $i "tmux-banner"
 done
