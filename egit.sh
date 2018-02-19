7fe79f982bb9fd1fbe2b2b24372da9c9ca49a75d
Roben Kleene <robenk@gmail.com>
2018-02-18 20:20:57 -0500

    Add separate _giterator file

--- egit.sh
+++ egit.sh
@@ -8,93 +8,10 @@ fi
 
 IFS=':' read -ra repos <<< "$EGITREPOS"
 
-usage () {
-  echo "Usage: egit [-pln]"
-  echo
-  echo "EGITREPOS environment variable defines witch directories to check."
-  echo
-  echo "No flags just lists the status of each repo."
-  echo "-p : Push all repos without staged changes"
-  echo "-l : Pull all repos without staged changes"
-  echo "-n : Print next directory path with unstaged changes"
-}
+command_name="egit"
 
-push=false
-pull=false
-next=false
-while getopts "plnh" option
-  do case "$option" in
-    p)
-      push=true
-      ;;
-    l)
-      pull=true
-      ;;
-    n)
-      next=true
-      ;;
-    h)
-      usage
-      exit 0
-      ;;
-    \?)
-      echo "Invalid option or missing argument"
-      usage
-      exit 1
-      ;;
-  esac
-done
-
-function git_process {
-  if ! [[ -d ".git" ]]; then
-    return
-  fi
-  nothing_to_commit=false
-  status=$(git status)
-
-  # Test git status message 1.
-  nothing_to_commit_message="nothing to commit (working directory clean)"
-  test "${status#*$nothing_to_commit_message}" != "$status" && nothing_to_commit=true
-  # Test git status message 2.
-  nothing_to_commit_message="nothing to commit, working directory clean"
-  test "${status#*$nothing_to_commit_message}" != "$status" && nothing_to_commit=true
-  # Test git status message 3.
-  nothing_to_commit_message="nothing to commit, working tree clean"
-  test "${status#*$nothing_to_commit_message}" != "$status" && nothing_to_commit=true
-
-
-  if $next; then
-    if ! $nothing_to_commit; then
-      pwd
-      break
-    fi
-  else
-    echo
-    pwd
-    git status
-  fi
-
-  if $push && $nothing_to_commit; then
-    git push
-  elif $pull && $nothing_to_commit; then
-    git pull
-  fi
-}
-
-function go_to_directory {
-  dir=$1
-  if [ -d "$dir" ]; then
-    cd "$1"
-    git_process
-  else
-    if ! $next; then
-      echo
-      echo "Directory does not exist $dir"
-    fi
-  fi
-}
+source ~/Development/Scripts/nobin/_giterator.sh
 
 for repo in "${repos[@]}"; do
   go_to_directory $repo
 done
-
