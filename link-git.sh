8422e7e5602fbf71387d7821c1ae3d734ab321f2
Roben Kleene <robenk@gmail.com>
2018-02-22 21:44:18 -0500

    Finish link-git

--- link-git.sh
+++ link-git.sh
@@ -16,39 +16,29 @@ commit=$(git rev-parse HEAD)
 
 remote=$(git config --get remote.origin.url | tr -d '\n')
 
-if $(echo $remote | grep \
-  --extended-regexp \
-  --only-matching \
-  --ignore-case \
-  --quiet \
-  '(https://|git@)github.com[/:]'); then
-  echo "GitHub"
-elif $(echo $remote | grep \
-  --extended-regexp \
-  --only-matching \
-  --ignore-case \
-  --quiet \
-  '(https://|git@)bitbucket.(com|org)[/:]'); then
-  echo "Bitbucket"
-else
-  echo "$remote is not a support remote" >&2
-  exit 1
-fi
-
 if [[ -n "$filename" ]]; then
   file_subpath=$(git ls-tree --full-name --name-only HEAD)
 fi
 
-
-
-# github_url = "https://github.com/#{github_subpath}"
-
-
-# cd directory && git ls-tree --full-name --name-only HEAD #{Shellwords.escape(filename)}
-
-# cd directory && git config --get remote.origin.url | cut -f2 -d: | cut -f1 -d.
-
-
-# github_subpath = URI.escape("#{repo_path}/blob/#{commit}/#{file_path}")
-# github_url = "https://github.com/#{github_subpath}"
-
+if [[ $remote =~ (https://|git@)github.com[/:](.*) ]]; then
+  remote_subpath="${BASH_REMATCH[2]}"
+  remote_subpath=${remote_subpath%.git}
+  repo_url="https://github.com/$remote_subpath"
+  if [[ -z "$file_subpath" ]]; then
+    echo $repo_url
+  else
+    echo "$repo_url/blob/$commit/$file_subpath"
+  fi
+elif [[ $remote =~ (https://|git@)bitbucket.(com|org)[/:](.*) ]]; then
+  remote_subpath="${BASH_REMATCH[3]}"
+  remote_subpath=${remote_subpath%.git}
+  repo_url="https://bitbucket.org/$remote_subpath"
+  if [[ -z "$file_subpath" ]]; then
+    echo $repo_url
+  else
+    echo "$repo_url/src/$commit/$file_subpath"
+  fi
+else
+  echo "$remote is not a support remote" >&2
+  exit 1
+fi
