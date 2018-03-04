f3de436c39aee50c16196cada522ec18d910d785
Roben Kleene <robenk@gmail.com>
2018-03-04 16:47:03 -0500

    Use reverse

--- nobin/_fzf-inline-result.sh
+++ nobin/_fzf-inline-result.sh
@@ -2,7 +2,7 @@ _fzf-inline-result() {
   local list_cmd=${1-$FZF_DEFAULT_COMMAND} 
   setopt localoptions pipefail 2> /dev/null
   if [ -z "$NVIM_LISTEN_ADDRESS" ]; then
-    export FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS"
+    export FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS"
   fi
   local result="$(eval "$list_cmd" |  fzf +m)"
   local ret=$?
