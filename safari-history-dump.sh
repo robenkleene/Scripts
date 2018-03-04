99639cf9d01e1f5fa87da600e8e0704d3b67cb9e
Roben Kleene <robenk@gmail.com>
2018-03-04 17:06:58 -0500

    Finish safari-history-dump

--- safari-history-dump.sh
+++ safari-history-dump.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sqlite3 -separator $'\t' ~/Library/Safari/History.db \
-  'SELECT title, url FROM history_visits INNER JOIN history_items ON history_visits.history_item = history_items.id ORDER BY visit_time desc LIMIT 1000;' \
+sqlite3 -noheader -separator $'\t' ~/Library/Safari/History.db \
+  'SELECT title, url FROM history_visits INNER JOIN history_items ON history_visits.history_item = history_items.id ORDER BY visit_time desc LIMIT 1000;' 2>/dev/null \
   | uniq
