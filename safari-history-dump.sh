6b2c4ae888132998ee1a87f40d8bfb0bc1373fea
Roben Kleene <robenk@gmail.com>
2018-03-04 20:29:09 -0500

    Break up long lines

--- safari-history-dump.sh
+++ safari-history-dump.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+read -r -d '' select << EOM
+SELECT title, url
+FROM history_visits
+INNER JOIN history_items
+ON history_visits.history_item = history_items.id
+ORDER BY visit_time desc
+LIMIT 1000;
+EOM
+
 sqlite3 -noheader -separator $'\t' ~/Library/Safari/History.db \
-  'SELECT title, url FROM history_visits INNER JOIN history_items ON history_visits.history_item = history_items.id ORDER BY visit_time desc LIMIT 1000;' 2>/dev/null \
+  "$select" 2>/dev/null \
   | uniq
