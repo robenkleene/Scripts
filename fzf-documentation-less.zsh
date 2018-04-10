edaa7832986eafa65382f312607aab53722c6d5c
Roben Kleene <robenk@gmail.com>
2018-04-10 10:24:30 -0400

    Refine less

--- fzf-documentation-less.zsh
+++ fzf-documentation-less.zsh
@@ -6,7 +6,7 @@ cd ~/Documentation/
 local result=$(_fzf-inline-result)
 if [[ -n $result ]]; then
   parameter=$(printf '%q' "$PWD/$result")
-  final_cmd="cat $parameter | less"
+  final_cmd="cat $parameter | less -FX"
   eval $final_cmd
   if [ $? -eq 0 ]; then
     # Add to history
